208fbb9ad00450a346fe2bda55930cf6ae6238ec
intel8008 <camster121@gmail.com>
2017-03-14 17:28:22 -0700

    Update Player.java
    Resolved compiler errors.

--- Player.java
+++ Player.java
@@ -54,5 +54,41 @@ public class Player {
 	public void setInHand(boolean inHand){
 		this.inHand = inHand;
 	}
+	
+	public long getHandValue(Game game) {
+		return PostFlopHandRanker.getAbsoluteHandStrength(getAllCards(game, this.holeCards));
+	}
+	
+	/**
+	 * Combines one player's hole Cards with any board cards to
+	 * form a single Card[]
+	 * @param game			The current Game object containing
+	 * 						all the game info.
+	 * @param holeCards		One player's hole cards
+	 * @return				A Card[] containing all the cards
+	 * 						currently visible to the player
+	 * 						whose hole cards are provided.
+	 */
+	public Card[] getAllCards(Game game, Card[] holeCards) {
+		int size = 2;
+		if (game.flopFlag) {
+			size += 3;
+		} 
+		if (game.riverFlag) {
+			size++;
+		}
+		if (game.turnFlag) {
+			size++;
+		}
+		Card[] allCards = new Card[size];
+		allCards[0] = holeCards[0];
+		allCards[1] = holeCards[1];
+		int index = 2;
+		while (index < size) {
+			allCards[index] = game.boardCards[index - 2];
+			index++;
+		}
+		return allCards;
+	}
 }
 
